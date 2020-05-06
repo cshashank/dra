3d7f166130ef0e7e06ccfe474d2f4d0e918e2376
cshashank <shashank@timbergrove.com>
2020-05-05 22:47:00 -0400

    new geo sql

---
+++ mssql/GeoQuery.sql
@@ -0,0 +1,10 @@
+select 
+	"USA" Country,LD.City,LD.Area State,LD.Description Depot,A.Description Account,LST.label ShipmentStatus,LSMA.Description ShipMethodActualDesc,P.ShipDate,P.ClearanceStatusType,
+    LSMO.Description ShipMethodOrgDesc,P.ShipMethodOriginal ShipMethodOrgCode,
+    P.CartonCount,P.PieceCount 
+from pieces P,accounts A ,lookupdepot LD, lookupstatustypes LST, lookupshipmethods LSMA,lookupshipmethods LSMO
+where P.AccountID=A.accountId 
+and P.AssignedDepotCode=LD.Code
+and P.StatusType=LST.StatusType
+and P.ShipMethodActual=LSMA.ShipMethodType
+and P.ShipMethodOriginal=LSMO.ShipMethodType
