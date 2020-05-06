4d51d924263c49fcad588007a7b03772479b99e7
cshashank <shashank@timbergrove.com>
2020-05-05 22:52:20 -0400

    checking new folder in repo

--- mssql/BatchTrackingDetail.sql
+++
@@ -1,6 +0,0 @@
-select 
-	P.PieceID,BP.OrderNumber,BP.BatchID,BP.Pin,B.BatchNumber, B.BatchType,B.Location
-from 
-	pieces P,batchpieces BP,batches B
-where
-		P.PieceID=BP.PieceID and BP.BatchID=B.BatchID  
